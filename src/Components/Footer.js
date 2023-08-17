7144937cada85545c77f016e9a2cdaed5ca77011
Raven Ramos <reigneraven.ramos@gmail.com>
2023-08-17 16:10:39 +0800

    modified footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import {FaGithub, FaLinkedin} from "react-icons/fa";
-import {GrMail} from "react-icons/gr";
-import {SiLeetcode} from "react-icons/si"
+import upang from '../images/upang.png';
 
 const Footer = () => {
   return (
     <footer>
-      <h4>Developed by Devansh Sahni</h4>
-      <h4>Copyright &copy; 2023 DS</h4>
-      <div className='footerLinks'>
-        <a href="https://github.com/DevanshSahni" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/devansh-sahni/" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:devanshsahni@gmail.com' target='_blank'><GrMail/></a>
-        <a href="https://leetcode.com/sahni1403/" target="_blank"><SiLeetcode/></a>
+      <div className="logo">
+        <img src={upang} alt="logo" height={100} width={100}/>
+        <h4>Phinma- University of Pangasinan</h4>
+        <p>Colllege of Information Technology</p>
       </div>
     </footer>
   )
